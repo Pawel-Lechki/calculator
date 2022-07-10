f5b39fff66daf2e6fac114f7c912bb4b300549da
Paweł Lechki <core.editor>
2022-07-10 23:49:58 +0200

    Add class Calculator and class function names

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
+class Calculator {
+  constructor(prevOperandTextElem, currOperandTextElem) {
+    this.prevOperandTextElem = prevOperandTextElem || document;
+    this.currOperandTextElem = currOperandTextElem;
+    this.clear();
+  }
+  clear() {
+    this.currOperand = "";
+    this.prevOperand = "";
+    this.operation = undefined;
+  }
+
+  delete() {}
+  appendNum(number) {
+    this.currOperand = number;
+  }
+  chooseOper(operation) {}
+
+  compute() {}
+
+  updateDisplay() {
+    this.currOperandTextElem.innerText = this.currOperand;
+  }
+}
+
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
 const deleteButton = document.querySelector("[data-delete]");
-const eprevOperandTextElem = document.querySelector("[data-pev]");
+const prevOperandTextElem = document.querySelector("[data-pev]");
 const currOperandTextElem = document.querySelector("[data-curr]");
 const clearButton = document.querySelector("[data-clear]");
+
+const claculator = new Calculator(prevOperandTextElem, currOperandTextElem);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    claculator.appendNum(button.innerHTML);
+    claculator.updateDisplay();
+  });
+});
