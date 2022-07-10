52b2330fcbba98186cbd66397b5b4feac03c75ef
Paweł Lechki <core.editor>
2022-07-10 23:38:29 +0200

    add js variables

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const eprevOperandTextElem = document.querySelector("[data-pev]");
+const currOperandTextElem = document.querySelector("[data-curr]");
+const clearButton = document.querySelector("[data-clear]");
